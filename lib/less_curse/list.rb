3b516f40be0c2651e150fce9b9c5d408856bffb7
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-09-04 00:24:25 +0200

    make demo show *something*

--- lib/less_curse/list.rb
+++ lib/less_curse/list.rb
@@ -2,5 +2,15 @@ module LessCurse
   class List
     attr_accessor :data
     attr_accessor :on_select
+
+    def refresh
+      window = LessCurse.screen.windows[self]
+      FFI::NCurses.box window, 0, 0
+      #FFI::NCurses.mvwaddstr(window, 0, 1, "Hello there")
+      @data.each_with_index do |d, idx|
+        FFI::NCurses.wmove window, idx + 1, 1
+        FFI::NCurses.waddstr window, d.to_s
+      end
+    end
   end
 end
--- lib/less_curse/screen.rb
+++ lib/less_curse/screen.rb
@@ -16,9 +16,29 @@ module LessCurse
 
     def add widget
       @widgets << widget
+      @windows[widget] = new_window
     end
 
     def show
+      #FFI::NCurses.initscr called in initialize
+      FFI::NCurses.cbreak # can ctrl-c
+      FFI::NCurses.noecho
+      FFI::NCurses.clear
+      FFI::NCurses.box @windows[@widgets[0]], 0, 0
+      FFI::NCurses.refresh
+      repaint
+    end
+
+    def repaint
+      FFI::NCurses.refresh
+      @widgets.each {|widget| widget.refresh}
+      @windows.each {|widget, window| FFI::NCurses.wrefresh window}
+    end
+
+    def new_window
+      # Should default to horizontal tiling
+      rectangle = LessCurse::Geometry::Rectangle.new 0, 0, 50, 15
+      LessCurse::window rectangle
     end
   end
 end
