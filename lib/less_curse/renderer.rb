15f963ca0968552185616a4dc67d297f03acdcd3
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-09-05 14:25:20 +0200

    Renderer helpers write_line, bold_if

--- lib/less_curse/renderer.rb
+++ lib/less_curse/renderer.rb
@@ -1,12 +1,28 @@
 module LessCurse
   module Renderer
+    # Draw box and title in top border of box
     def self.box_with_title window, title
       FFI::NCurses.box window, 0, 0
       FFI::NCurses.mvwaddstr window, 0, 1, title
     end
+    # Draw into lower border of a box
     def self.box_foot window, text
       height,width = FFI::NCurses::getmaxyx(window)
       FFI::NCurses.mvwaddstr window, height - 1, 1, text
+    end
+    # Write a line in window
+    def self.write_line window, line_number, text
+      FFI::NCurses.mvwaddstr window, line_number + 1, 1, text
+    end
+    # Switch on boldness depending on condition
+    def self.bold_if condition, window
+      if condition
+        FFI::NCurses.wattron window, FFI::NCurses::A_BOLD
+      end
+      yield
+      if condition
+        FFI::NCurses.wattroff window, FFI::NCurses::A_BOLD
+      end
     end
   end
 end
