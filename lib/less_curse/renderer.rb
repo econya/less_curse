0410b8e5dd3e46ef446c9def344649a8aa825363
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-09-05 09:29:08 +0200

    add Renderer::box_foot helper

--- lib/less_curse/renderer.rb
+++ lib/less_curse/renderer.rb
@@ -4,5 +4,9 @@ module LessCurse
       FFI::NCurses.box window, 0, 0
       FFI::NCurses.mvwaddstr window, 0, 1, title
     end
+    def self.box_foot window, text
+      height,width = FFI::NCurses::getmaxyx(window)
+      FFI::NCurses.mvwaddstr window, height - 1, 1, text
+    end
   end
 end
