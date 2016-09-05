4ea9ae9df02a7b39f6a3653ee1448fb3594237d4
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-09-05 09:29:56 +0200

    Show interface in Widget::Base

--- lib/less_curse/widgets/base.rb
+++ lib/less_curse/widgets/base.rb
@@ -7,6 +7,16 @@ module LessCurse
       def initialize data: nil, title: ""
         @data, @title = data, title
       end
+
+      # Refresh portions of screen, probably using ncurses primitives
+      def refresh
+        false
+      end
+
+      # Handle input or return false if doesnt care
+      def handle_input
+        false
+      end
     end
   end
 end
