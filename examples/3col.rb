e289b52ba8165bb811259ee6b5c123e6c6223bd3
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-09-18 17:19:18 +0200

    add 3col example

---
+++ examples/3col.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+# Released under the GPLv3+, Copyright 2016 Felix Wolfsteller
+
+require 'less_curse'
+
+def list_ui
+  simple_list = [1, 2, 3, 4, 5, 6, 7]
+
+  list_ui = LessCurse::Widgets::List.new
+  list_ui.data = simple_list
+  list_ui.on_select = LessCurse::Actions::QUIT
+  list_ui
+end
+
+def hash_ui
+  simple_hash = {rainbows: "fabolous",
+                 unicorns: "magnificient",
+                 sunrise:  "fantastic",
+                 sunset:   "marvellous"}
+  LessCurse::Widgets::List.new data: simple_hash, title: "Hash keys"
+end
+
+def textview_ui
+  LessCurse::Widgets::TextView.new title: 'TextView',
+                                   data: "Showing Text Data\nSecond Line of Text Data"
+end
+
+begin
+  # Display list items
+  LessCurse.screen.add list_ui
+
+  # Display hash items
+  LessCurse.screen.add hash_ui
+
+  # Display important information
+  LessCurse.screen.add textview_ui
+
+  # Display it, really
+  LessCurse.show_screen
+
+  # Deal with keyboard input and keep us living
+  LessCurse.enter_loop!
+
+  # Try to debug
+  LessCurse.screen.windows.each do |widget, window|
+    puts "#{widget} -> #{window}"
+  end
+ensure
+  LessCurse.close_screen
+end
