6d1f26a75927560829ee9e90039b243aae568493
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-09-23 09:11:02 +0200

    whitespaces between methods.

--- lib/less_curse/renderer.rb
+++ lib/less_curse/renderer.rb
@@ -5,15 +5,18 @@ module LessCurse
       FFI::NCurses.box window, 0, 0
       FFI::NCurses.mvwaddstr window, 0, 1, title
     end
+
     # Draw into lower border of a box
     def self.box_foot window, text
       height,width = FFI::NCurses::getmaxyx(window)
       FFI::NCurses.mvwaddstr window, height - 1, 1, text
     end
+
     # Write a line in window
     def self.write_line window, line_number, text
       FFI::NCurses.mvwaddstr window, line_number + 1, 1, text
     end
+
     # Switch on boldness depending on condition
     def self.bold_if condition, window
       if condition
