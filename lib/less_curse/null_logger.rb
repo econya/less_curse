3a6edae82340af42c01fb705a7cd84b4f15c5966
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-09-23 09:17:48 +0200

    move comment about NullLogger into LessCurse namespace where it belongs to.

--- lib/less_curse/null_logger.rb
+++ lib/less_curse/null_logger.rb
@@ -1,7 +1,7 @@
 require 'logger'
 
-# NullLogger â la hawkins: http://hawkins.io/2013/08/using-the-ruby-logger/
 module LessCurse
+  # NullLogger â la hawkins: http://hawkins.io/2013/08/using-the-ruby-logger/
   class NullLogger < Logger
     def initialize(*args)  ; end
     def add(*args, &block) ; end
